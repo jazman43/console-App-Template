56abed8d1370abfef9c344edf57a4340787f20c1
jazman43 <supoww@hotmail.co.uk>
2022-12-15 21:34:42 +1300

    work-on-windowIniti-functions

--- CS103_Project_school_infomation_system/CS103_Project_school_infomation_system/main.cpp
+++ CS103_Project_school_infomation_system/CS103_Project_school_infomation_system/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "windowInti.h"
 
 
 
@@ -7,6 +7,23 @@
 int main() {
 	//main loop will call one function that will handle quiting starting menu and will include header files needed.
 
+	WindowInti consoleCreate;	
+	
+
+	//clears console else will get wied symbols
+	consoleCreate.ClearConsole();
+	
+	while (true)
+	{
+		//Draw Text wstring fucntion tack an x and y position and a wide string (L"text") upper case L must come frist 
+		consoleCreate.DrawTextWString(10, 10,L"hello");
+
+		//currenty repeting bug?
+		consoleCreate.DrawLine(0, 2, 20, 40, '.');
+
+		//updates console every frame
+		consoleCreate.Update();
+	}
 
 	return 0;
 }
--- CS103_Project_school_infomation_system/CS103_Project_school_infomation_system/windowInti.h
+++ CS103_Project_school_infomation_system/CS103_Project_school_infomation_system/windowInti.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <windows.h>
+#include <string>
 
+#ifndef UNICODE
+#error Please enable UNICODE for your compiler! VS: Project Properties -> General -> \
+Character Set -> Use Unicode.
+#define UNICODE
+#define _UNICODE
+#endif
 
 
 class WindowInti {
@@ -8,11 +15,24 @@ public:
 	WindowInti();
 	~WindowInti();
 
+	void Update();
+
+	void DrawTextWString(int x, int y, std::wstring text);
+
+	void ClearConsole();
+
+	virtual void Draw(int x, int y, short symbol);
+
+	void DrawLine(int x1, int y1, int x2, int y2, short symbol = 0x2588);
+
 private:
 		
 	int width = 120;
 	int height = 40;
+	wchar_t* screen;
+
+	DWORD dwBytesWitten;
 	
 
-
+	HANDLE handleConsole;
 };
